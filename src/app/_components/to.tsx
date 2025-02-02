cc828386f90d8b74c4a6c757e65645ab340cc57a
Devan Huapaya <huapayadevan@gmail.com>
2025-02-01 17:39:59 -0800

    🧩 Add To component

---
+++ src/app/_components/to.tsx
@@ -0,0 +1,21 @@
+import cn from 'classnames'
+
+type Props = {
+  children?: React.ReactNode;
+  className?: string;
+  blank?: boolean
+  a: string
+};
+
+const To = ({ children, className, blank=true, a }: Props) => {
+  return <a
+     data-testid="To" 
+     className={cn("container mx-auto px-5 lg:px-0", className)}
+      href={a} 
+      target={blank ? '_blank' : undefined}
+    >
+      {children}
+    </a>;
+};
+
+export default To;
