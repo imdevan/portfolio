6643eddc972b6132afb718e010e02b82023a8dc3
Devan Huapaya <huapayadevan@gmail.com>
2025-02-02 09:51:13 -0800

    💅 Move container on index page

--- src/app/_components/intro.tsx
+++ src/app/_components/intro.tsx
@@ -5,36 +5,40 @@ import classNames from "classnames";
 import Portrait from "./portrait";
 const firaCode = Fira_Code({ subsets: ["latin"] });
 import AnimateIn from "_c/animate-in";
+import Container from "./container";
 
 export function Intro() {
   return (
-    <section className="flex-col flex items-center md:justify-between mt-16 mb-16 md:mb-32 max-w-6xl mx-auto">
+    <section className="flex-col flex items-center md:justify-between mt-16 mb-16 md:mb-32 max-w-6xl mx-auto w-full overflow-x-hidden">
       <AnimateIn>
-        <div className="md:mt-8 mb-32 md:mb-32">
-          <AppTitle>
-            Devan Huapaya
-          </AppTitle>
+        <div className="mt-10 md:mt-8 mb-32 md:mb-32">
+          <Container>
+            <AppTitle>
+              Devan Huapaya
+            </AppTitle>
+          </Container>
         </div>
 
         <AnimateIn>
           <div className="mb-24">
-              <div className="flex justify-center items-center">
+              <div className="flex justify-center items-center" >
                 <Portrait />
               </div>
           </div>
 
             <AnimateIn>
-              <H s={2} className={classNames(firaCode.className, "text-2xl md:text-3xl")}> 
-                <AnimateIn>
-                  <span className="dark:text-pink-200 text-pink-400">
-                    Full-stack Typescript Developer.
-                  </span>    
-
+              <Container>
+                <H s={2} className={classNames(firaCode.className, "text-2xl md:text-3xl")}>
                   <AnimateIn>
-                    That builds web and native apps, deploys them to production, and cares about people.
+                    <span className="dark:text-pink-200 text-pink-400">
+                      Full-stack Typescript Developer.
+                    </span>
+                    <AnimateIn>
+                      That builds web and native apps, deploys them to production, and cares about people.
+                    </AnimateIn>
                   </AnimateIn>
-                </AnimateIn>
-              </H>
+                </H>
+              </Container>
             </AnimateIn>
         </AnimateIn>
       </AnimateIn>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Container from "_c/container";
 import { Intro } from "_c/intro";
 import { Works } from "_c/works";
 import { getAllProjects } from "@/lib/api";
@@ -8,12 +7,12 @@ export default function Index() {
   const allProjects = getAllProjects();
 
   return (
-    <Container>
+    <>
       <Header link={false} />
   
       <Intro />
   
       {allProjects.length > 0 && <Works projects={allProjects} />}
-    </Container>
+    </>
   );
 }
