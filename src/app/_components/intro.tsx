72b378418acfe072c5b27bf4f78e4c9e222e7276
Devan Huapaya <huapayadevan@gmail.com>
2025-02-02 10:06:32 -0800

    💅 Fix overflow issue

--- src/app/_components/intro.tsx
+++ src/app/_components/intro.tsx
@@ -9,7 +9,7 @@ import Container from "./container";
 
 export function Intro() {
   return (
-    <section className="flex-col flex items-center md:justify-between mt-16 mb-16 md:mb-32 max-w-6xl mx-auto w-full overflow-x-hidden">
+    <section className="flex-col flex items-center md:justify-between mt-16 mb-16 md:mb-32 max-w-6xl mx-auto w-full overflow-x-hidden overflow-y-hidden">
       <AnimateIn>
         <div className="mt-10 md:mt-8 mb-32 md:mb-32">
           <Container>
