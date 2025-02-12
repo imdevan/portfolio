c251fe715cf65b5654c27f3d16af95c33959ef55
Devan Huapaya <huapayadevan@gmail.com>
2025-02-12 13:32:22 -0800

    💅 add opacity to header background

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -10,7 +10,8 @@ type Props = {
 
 const Header = ({link = true}: Props) => {
   return (
-    <div className="fixed top-0 left-0 bg-gradient-to-b dark:from-zinc-900 from-white to-transparent dark:text-neutral-50 transition-colors duration-200 w-full z-50">
+    <>
+      <div className="fixed top-0 left-0 transition-colors duration-200 w-full z-50">
       <div className="container p-8">
         <h2 className={cn(robotoSlab.className, "text-xl md:text-xl font-bold tracking-tight")}>
           {link ? <Link href="/" className="hover:underline">
@@ -19,6 +20,14 @@ const Header = ({link = true}: Props) => {
         </h2>
       </div>
     </div>
+    <div className="fixed top-0 left-0 bg-gradient-to-b dark:from-zinc-900 from-white to-transparent dark:text-neutral-50 transition-colors duration-200 w-full z-40 opacity-90 dark:opacity-95">
+      <div className="container p-8">
+        <h2 className={cn(robotoSlab.className, "text-xl md:text-xl font-bold tracking-tight")}>
+          {' '}
+        </h2>
+      </div>
+    </div>
+    </>
   );
 };
 
